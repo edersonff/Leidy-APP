73042378a7e3db02a722836001567ec59a336255
edersonff <=>
2022-06-26 10:51:43 -0300

    Using inter font

---
+++ App.style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import { fonts } from "./styles/base";
+
+export default StyleSheet.create({
+    app_container:{
+        fontFamily: fonts.primary,
+        flex: 1
+    }
+});
