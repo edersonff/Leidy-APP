f66f794f0bfffe50b74c0d8eabf908a47d25104c
eder <fulanodetal@exemplo.br>
2022-07-05 12:27:13 -0300

    Label changed in input component

--- Components/Input.js
+++ Components/Input.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components/native'
 import { View } from 'react-native'
+import Animation from '../Helpers/Animation';
+import { useState, useEffect } from 'react';
 
-export default function Input({secure, name, onChange, placeholder}){
+export default function Input({secure, onChange, placeholder}){
+    const [selected, setSelected] = useState(false);
+    let top = Animation(0,50,250);
+    useEffect(()=>{
+        top.setValue(selected ? 50 : 0);
+    }, [selected])
     return(
         <Container>
-            <Label>{name}</Label>
-            <InputText secureTextEntry={secure} placeholderTextColor={'rgb(230,230,230)'} onChange={onChange} placeholder={placeholder} />
+            <Label style={{top}}>{placeholder}</Label>
+            <InputText onfocus={() => {  }} on secureTextEntry={secure} placeholderTextColor={'rgb(230,230,230)'} onChange={onChange} />
         </Container>
     )
 }
@@ -14,13 +21,14 @@ const Container = styled.View`
     fontSize: 1.2em;
 `
 const Label = styled.Text`
-    fontSize: 1em;
-    color: rgb(100,100,100);
+    fontSize: 0.8em;
+    color: rgb(200,200,200);
+    position: relative;
 `
 const InputText = styled.TextInput`
     outlineStyle: none;
     padding: 13px 40px;
-    textAlign: center;
+    textAlign: left;
     border-bottom-width: 1px;
     border-bottom-style : solid;
     border-bottom-color : #000;
