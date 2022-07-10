ea6e84d4929590dd255f3a603c5c3f5c818a9d3d
edersonff <=>
2022-07-10 20:43:20 -0300

    Horizontal layout added

---
+++ Components/HorizontalScroll.js
@@ -0,0 +1,25 @@
+import {  View, ScrollView, StyleSheet } from 'react-native';
+
+export default function HorizontalScroll({children, marginTop = 10}){
+    const styles = StyleSheet.create({
+        menu: {
+          flexGrow: 1,
+        },
+        menu_container:{
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            flexDirection: 'row',
+            flex: 1,
+            height: '100%',
+        }, 
+    })
+    return(
+        <View style={{ marginTop }}>
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal={true} contentContainerStyle={styles.menu}>
+                <View style={ styles.menu_container }>
+                    {children}
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
