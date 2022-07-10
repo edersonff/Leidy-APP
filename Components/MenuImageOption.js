7dc9271312a86439ef01ecf0d06cf6419effcae4
edersonff <=>
2022-07-10 20:43:56 -0300

    Images menu option added

---
+++ Components/MenuImageOption.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native'
+
+export default function MenuImageOption({text, src, color = '#000', first}){
+    const Container = styled.View`
+        fontSize: 1.2em;
+        margin: 0 15px;
+        margin-left: ${first ? '0' : 'auto'};
+    `
+    const MenuImage = styled.View`
+        textAlign: left;
+    `
+    const Image = styled.Image`
+        resize-mode: contain;
+        width: 90px;
+        height: 90px;
+        border-radius: 9px;
+    `
+    const ImageView = styled.View`
+        textAlign: left;
+    `
+    const Text = styled.Text`
+        color: ${ color };
+        font-size: 12px;
+        text-align: center;
+        font-weight: 700;
+    `
+    return(
+        <Container>
+            <MenuImage>
+                <ImageView>
+                    <Image source={src}/>
+                </ImageView>
+                <Text>{text}</Text>
+            </MenuImage>
+        </Container>
+    )
+}
