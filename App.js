c86d8f851b71e40bf9188c5bd2e58a97be3f4cdf
edersonff <=>
2022-07-10 20:43:32 -0300

    New screens added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import Perfil from './Screens/Perfil/Perfil';
 import Loading from './Screens/Loading/Loading';
 import AuthPage from './Screens/AuthPage/AuthPage';
+import Dashboard from './Screens/Dashboard/Dashboard';
 import SystemMessage from './Screens/SystemMessage/SystemMessage';
 import style from './App.style';
 import { createStackNavigator  } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createStackNavigator();
 
@@ -13,17 +15,25 @@ export default function App() {
   return (
     <NavigationContainer style={style.app_container}>
       <Stack.Navigator
-        initialRouteName='Loading'
+        initialRouteName='Perfil'
         screenOptions={{
           headerShown: false,
           animationEnabled: true
         }}>
+
         <Stack.Screen name="Loading" component={Loading} options={{ gestureDirection: 'horizontal' }} />
 
         <Stack.Screen name="AuthPage" component={AuthPage} />
         
         <Stack.Screen name="SystemMessage" component={SystemMessage} />
+        
+        <Stack.Group>
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen name="Perfil" component={Perfil} />
+        </Stack.Group>
+
       </Stack.Navigator>
+      <StatusBar/>
     </NavigationContainer>
   );
 }
--- App.style.js
+++ App.style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     app_container:{
         flex: 1,
-        fontFamily: '"Inter", sans-seri',
+        fontFamily: 'Inter", sans-serif',
         display: "none"
     }
 });
